2889ec1f3465e67170b61f86f6b55de606c34210
JYOTHISWARUP E V <jyothiswarup99@gmail.com>
2024-03-25 20:27:44 +0530

    done

--- src/components/pages/userDashboard/UserDashboard.jsx
+++ src/components/pages/userDashboard/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { movies} from "../../Contex";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UserNavbar from "../../Header/UserNavbar";
 
 const UserDashboard = () => {
